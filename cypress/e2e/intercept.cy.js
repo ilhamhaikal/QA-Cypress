9e9ce66bb07a725719eda1ba36cbe6c3a05250e8
PH315-53 <m.ilhamhaikal13@gmail.com>
2024-11-26 13:27:20 +0700

    Menambahkan file baru

--- cypress/e2e/intercept.cy.js
+++ cypress/e2e/intercept.cy.js
@@ -9,14 +9,16 @@ describe('OrangeHRM Login Page Tests', () => {
     cy.get('img[alt="company-branding"]').should('be.visible'); // Logo terlihat
     cy.get('input[name="username"]').should('be.visible'); // Input username terlihat
     cy.get('input[name="password"]').should('be.visible'); // Input password terlihat
-    cy.get('button[type="submit"]').should('be.visible').and('contain', 'Login'); // Tombol login terlihat
+    cy.get('button[type="Submit"]').should('be.visible').and('contain', 'Login'); // Tombol login terlihat
   });
 
   it('should successfully login with valid credentials', () => {
     // Isi username dan password valid
+    cy.intercept('GET', 'https://opensource-demo.orangehrmlive.com/web/index.php/api/v2/dashboard/employees/action-summary').as('actionsummary');
     cy.get('input[name="username"]').type('Admin');
     cy.get('input[name="password"]').type('admin123');
-    cy.get('button[type="submit"]').click();
+    cy.get('button[type="Submit"]').click();
+    cy.wait('@actionsummary');
 
     // Verifikasi login berhasil
     cy.url().should('include', '/dashboard/index'); // URL berubah ke dashboard
@@ -29,60 +31,61 @@ describe('OrangeHRM Login Page Tests', () => {
     cy.get('input[name="password"]').type('invalidPassword');
     cy.get('button[type="submit"]').click();
 
-    // Verifikasi pesan error muncul
+  
     cy.get('.oxd-alert-content')
       .should('be.visible')
-      .and('contain', 'Invalid credentials');
+      .and('contain', 'Invalid credentials'); // Verifikasi pesan error
   });
 
   it('should show error for invalid username and valid password', () => {
     cy.get('input[name="username"]').type('InvalidUser'); // Masukkan username salah
     cy.get('input[name="password"]').type('admin123'); // Masukkan password valid
     cy.get('button[type="submit"]').click(); // Klik tombol login
-  
-    // Verifikasi pesan error muncul
+
+
     cy.get('.oxd-alert-content').should('be.visible').and('contain', 'Invalid credentials');
   });
 
   it('should show validation error for empty username', () => {
     cy.get('input[name="password"]').type('admin123'); // Masukkan password
     cy.get('button[type="submit"]').click(); // Klik tombol login
-  
-    // Verifikasi pesan validasi
+
+    // Verifikasi pesan validasi tanpa request
     cy.get('.oxd-input-group__message').eq(0).should('contain', 'Required'); // Validasi username
   });
 
   it('should show validation errors for empty fields', () => {
-      // Klik tombol login tanpa mengisi form
-      cy.get('button[type="submit"]').click();
-    
-      // Verifikasi pesan validasi username
-      cy.contains('span.oxd-input-group__message', 'Required', { timeout: 2000 })
-        .should('be.visible'); // Pesan validasi harus terlihat
-    
-      // Verifikasi pesan validasi password
-      cy.contains('span.oxd-input-group__message', 'Required', { timeout: 2000 })
-        .should('be.visible'); // Pesan validasi harus terlihat
-    });
+    // Klik tombol login tanpa mengisi form
+    cy.get('button[type="submit"]').click();
+
+    // Verifikasi pesan validasi username
+    cy.contains('span.oxd-input-group__message', 'Required', { timeout: 2000 }).should('be.visible'); // Pesan validasi username
+
+    // Verifikasi pesan validasi password
+    cy.contains('span.oxd-input-group__message', 'Required', { timeout: 2000 }).should('be.visible'); // Pesan validasi password
+  });
 
   it('should mask the password field', () => {
     // Pastikan input password tersamarkan
     cy.get('input[name="password"]').should('have.attr', 'type', 'password');
   });
-  
+
   it('should navigate to the Forgot Password page when the link is clicked', () => {
-      // Klik tombol "Forgot your password?"
-      cy.contains('Forgot your password?').click();
-    
-      // Verifikasi URL setelah navigasi
-      cy.url().should('include', '/auth/requestPasswordResetCode');
-    
-      // Verifikasi bahwa halaman "Forgot Password" ditampilkan
-      cy.get('h6').should('contain', 'Reset Password'); // Pastikan elemen judul halaman sesuai
-    });
+    // Klik tombol "Forgot your password?"
+    cy.contains('Forgot your password?').click();
 
-    it('should verify the target page of OrangeHRM, Inc.', () => {
-      cy.request('https://www.orangehrm.com/').its('status').should('eq', 200); // Pastikan halaman dapat diakses
-    });
+    // Verifikasi URL setelah navigasi
+    cy.url().should('include', '/auth/requestPasswordResetCode');
 
+    // Verifikasi bahwa halaman "Forgot Password" ditampilkan
+    cy.get('h6').should('contain', 'Reset Password'); // Pastikan elemen judul halaman sesuai
+  });
+
+  it('should verify the target page of OrangeHRM, Inc.', () => {
+    // Intercept halaman eksternal
+    cy.intercept('GET', 'https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index').as('homepageRequest');
+    cy.request('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index').its('status').should('eq', 200);
+
+    // cy.wait('@homepageRequest').its('response.statusCode').should('eq', 200);
+  });
 });
